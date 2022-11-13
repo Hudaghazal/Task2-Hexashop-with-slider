2c22c0ea2cde865181ffcbb4527119dcb3ebc67d
Hudaghazal <hudaygh2000@gmail.com>
2022-11-13 12:42:11 +0300

    first commit

---
+++ assets/js/java.js
@@ -0,0 +1,9 @@
+$('.multiple-items').slick({
+     infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<i class="iconb"> </i>',
+    nextArrow:  '<i class="icona"> </i>'
+  });
+  
+  
